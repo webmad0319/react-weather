208b1976fb46473316dbebe47bc987814851d287
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-05-01 00:56:22 +0200

    no bonus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import cities from './cities.json'
+import City from './components/city/City'
+import "./weather-icons-master/css/weather-icons.css"
 import './App.css';
 
 class App extends React.Component {
@@ -9,19 +12,49 @@ class App extends React.Component {
       "Oceania",
       "Africa",
       "Asia"
-    ]
+    ],
+    cities: []
   }
 
+
+  showCities(continent) {
+    let allThecities = [...cities];
+    let citiesContinent = allThecities.filter(city => city.continent === continent);
+
+    this.setState({
+      ...this.state,
+      cities: citiesContinent
+    })
+
+  }
+
+
   render() {
+    const buttonContinent = 
+      this.state.continents.map((continents,i) => {
+        return (
+          <React.Fragment key={i}>
+            <button className="btn" type="button" onClick={() => this.showCities(continents)}>{[...continents]}</button>
+          </React.Fragment>
+        )
+      });
+    const showCieties = 
+    this.state.cities.map((cities,i) => {
+      return (
+        <React.Fragment key={i}>
+          <City {...cities} />
+        </React.Fragment>
+      )
+    });  
     return (
       <React.Fragment>
         <nav>
           <ul>
-            <li>Continent buttons go here (replace this li with a loop)</li>
+            <li>{buttonContinent}</li>
           </ul>
         </nav>
-        <section>
-          Weather widgets go here
+        <section className="weatherSection">
+          {showCieties}
         </section>
       </React.Fragment>
     )
