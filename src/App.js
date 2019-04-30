e35d6da374e54cfea49d43ad15d561f986c6645a
Dani Vicario <danigv.email@gmail.com>
2019-04-30 14:14:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    continents: [
+      "America",
+      "Europe",
+      "Oceania",
+      "Africa",
+      "Asia"
+    ]
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <nav>
+          <ul>
+            <li>Continent buttons go here (replace this li with a loop)</li>
+          </ul>
+        </nav>
+        <section>
+          Weather widgets go here
+        </section>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
