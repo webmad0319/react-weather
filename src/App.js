45506dfe0e8bfd8a538ddeb7766032192887cfd2
Luca Manfredi <luca.manfredi1987@gmail.com>
2019-05-01 09:36:29 +0200

    miss gradient

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import chroma from 'chroma-js';
 import cities from './cities.json'
 import City from './components/city/City'
 import "./weather-icons-master/css/weather-icons.css"
@@ -13,17 +14,28 @@ class App extends React.Component {
       "Africa",
       "Asia"
     ],
-    cities: []
+    cities: [],
+    average : 0,
+    temperature : []
   }
 
 
   showCities(continent) {
     let allThecities = [...cities];
-    let citiesContinent = allThecities.filter(city => city.continent === continent);
+    let citiesContinent = allThecities.filter(city => city.continent === continent).sort((a,b) => a.temperature - b.temperature);
+    let totalAverage = citiesContinent.reduce((a,b) => {
+        return a + b.temperature
+    }, 0) / citiesContinent.length;
+    let temperature = citiesContinent.map((city) => city.temperature);
+
+    let colorScale = chroma.scale(['rgb(0,0,255)', 'rgb(255,0,0)']).domain([temperature[0], temperature[temperature.length-1]]);
+    let color = colorScale(cities);
 
     this.setState({
       ...this.state,
-      cities: citiesContinent
+      cities: citiesContinent,
+      average: totalAverage,
+      temperature: temperature
     })
 
   }
@@ -56,6 +68,7 @@ class App extends React.Component {
         <section className="weatherSection">
           {showCieties}
         </section>
+        <h2 className="average">Total average:{this.state.average}</h2>
       </React.Fragment>
     )
   }
